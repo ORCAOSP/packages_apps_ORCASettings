6cb43f751d3a6fb96fead1c5dfc92c474d7b2727
Drew Garen <orcaproject@yahoo.com>
2013-03-25 15:05:38 -0400

    ORCASettings

---
+++ src/com/aokp/romcontrol/fragments/PowerMenu.java
@@ -0,0 +1,108 @@
+
+package com.aokp.romcontrol.fragments;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+
+import com.aokp.romcontrol.AOKPPreferenceFragment;
+import com.aokp.romcontrol.R;
+import com.aokp.romcontrol.R.xml;
+
+public class PowerMenu extends AOKPPreferenceFragment {
+
+    //private static final String PREF_POWER_SAVER = "show_power_saver";
+    private static final String PREF_SCREENSHOT = "show_screenshot";
+    private static final String PREF_TORCH_TOGGLE = "show_torch_toggle";
+    private static final String PREF_AIRPLANE_TOGGLE = "show_airplane_toggle";
+    private static final String PREF_NAVBAR_HIDE = "show_navbar_hide";
+    private static final String PREF_REBOOT_KEYGUARD = "show_reboot_keyguard";
+
+    //CheckBoxPreference mShowPowerSaver;
+    CheckBoxPreference mShowScreenShot;
+    CheckBoxPreference mShowTorchToggle;
+    CheckBoxPreference mShowAirplaneToggle;
+    CheckBoxPreference mShowNavBarHide;
+    CheckBoxPreference mShowRebootKeyguard;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.prefs_powermenu);
+        /*
+        mShowPowerSaver = (CheckBoxPreference) findPreference(PREF_POWER_SAVER);
+        int powerSaverVal = 0;
+        try {
+            powerSaverVal = Settings.Secure.getInt(mContentRes,
+             Settings.Secure.POWER_SAVER_MODE);
+        } catch (SettingNotFoundException e) {
+            mShowPowerSaver.setEnabled(false);
+            mShowPowerSaver
+                    .setSummary("You need to enable power saver before you can see it in the power menu.");
+        }
+        mShowPowerSaver.setChecked(powerSaverVal != 0); */
+
+        mShowTorchToggle = (CheckBoxPreference) findPreference(PREF_TORCH_TOGGLE);
+        mShowTorchToggle.setChecked(Settings.System.getBoolean(mContentRes,
+                Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE, false));
+
+        mShowScreenShot = (CheckBoxPreference) findPreference(PREF_SCREENSHOT);
+        mShowScreenShot.setChecked(Settings.System.getBoolean(mContentRes,
+                Settings.System.POWER_DIALOG_SHOW_SCREENSHOT, false));
+
+        mShowAirplaneToggle = (CheckBoxPreference) findPreference(PREF_AIRPLANE_TOGGLE);
+        mShowAirplaneToggle.setChecked(Settings.System.getBoolean(mContentRes,
+                Settings.System.POWER_DIALOG_SHOW_AIRPLANE_TOGGLE, true));
+
+        mShowNavBarHide = (CheckBoxPreference) findPreference(PREF_NAVBAR_HIDE);
+        mShowNavBarHide.setChecked(Settings.System.getBoolean(mContentRes,
+                Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE, false));
+
+        mShowRebootKeyguard = (CheckBoxPreference) findPreference(PREF_REBOOT_KEYGUARD);
+        mShowRebootKeyguard.setChecked(Settings.System.getBoolean(mContentRes,
+                Settings.System.POWER_DIALOG_SHOW_REBOOT_KEYGUARD, true));
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mShowScreenShot) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_SCREENSHOT,
+                    ((CheckBoxPreference)preference).isChecked());
+            return true;
+        /*
+        } else if (preference == mShowPowerSaver) {
+            Settings.System.putInt(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_POWER_SAVER,
+                    ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
+            return true; */
+        } else if (preference == mShowTorchToggle) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE,
+                    ((CheckBoxPreference)preference).isChecked());
+            return true;
+        } else if (preference == mShowAirplaneToggle) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_AIRPLANE_TOGGLE,
+                    ((CheckBoxPreference)preference).isChecked());
+            return true;
+        } else if (preference == mShowNavBarHide) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE,
+                    ((CheckBoxPreference)preference).isChecked());
+            return true;
+        } else if (preference == mShowRebootKeyguard) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_REBOOT_KEYGUARD,
+                    ((CheckBoxPreference)preference).isChecked());
+            return true;
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+}
