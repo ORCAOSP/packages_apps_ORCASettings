ff4c575d14cf9ea59272ff731fa38f1c4d8a6fed
orcaosp <orcaproject@yahoo.com>
2013-04-22 19:01:05 -0400

    updated bigfoot settings based on aokp rom control

--- src/com/aokp/romcontrol/fragments/StatusBarToggles.java
+++ src/com/aokp/romcontrol/fragments/StatusBarToggles.java
@@ -68,6 +68,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class StatusBarToggles extends AOKPPreferenceFragment implements
@@ -94,6 +95,8 @@ public class StatusBarToggles extends AOKPPreferenceFragment implements
     private final int PICK_CONTACT = 1;
 
     public static final int REQUEST_PICK_CUSTOM_ICON = 200;
+
+    private static boolean mTogglesAreSorted = false;
 
     private Resources mResources;
     private PackageManager mPackMan;
@@ -910,6 +913,10 @@ public class StatusBarToggles extends AOKPPreferenceFragment implements
             for (String toggle : splitter) {
                 userEnabledToggles.add(toggle);
             }
+            if (!mTogglesAreSorted) {
+                Collections.sort(userEnabledToggles);
+                mTogglesAreSorted = true;
+            }
             return userEnabledToggles;
         } catch (Exception e) {
             if (sToggles != null && sToggles.containsKey("default_toggles")) {
@@ -929,8 +936,10 @@ public class StatusBarToggles extends AOKPPreferenceFragment implements
             b.append(_toggle);
             b.append("|");
         }
-        if (String.valueOf(b.charAt(b.length() - 1)).equals("!")) {
-            b.deleteCharAt(b.length() - 1);
+        if (b.length() > 0) {
+            if (b.charAt(b.length() - 1) == '!') {
+                b.deleteCharAt(b.length() - 1);
+            }
         }
         Log.d(TAG, "saving toggles:" + b.toString());
         Settings.System.putString(c.getContentResolver(), Settings.System.QUICK_TOGGLES,
