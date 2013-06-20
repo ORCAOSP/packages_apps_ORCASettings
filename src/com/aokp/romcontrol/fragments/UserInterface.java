b7962fe1ef72f8a3d4ac9238b0a90463a3a217a7
Orca Project <shamukillaz@orcaproject.co>
2013-06-19 22:39:04 -0400

    Remove Allow user selectable StatusBar FontSize
    This will fix Statusbar Icons DPI

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -107,7 +107,6 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     private static final CharSequence PREF_HIDDEN_STATUSBAR_PULLDOWN = "hidden_statusbar_pulldown";
     private static final CharSequence PREF_HIDDEN_STATUSBAR_PULLDOWN_TIMEOUT = "hidden_statusbar_pulldown_timeout";
 
-    private static int STOCK_FONT_SIZE = 16;
     private static final int REQUEST_PICK_WALLPAPER = 201;
     //private static final int REQUEST_PICK_CUSTOM_ICON = 202; //unused
     private static final int REQUEST_PICK_BOOT_ANIMATION = 203;
@@ -142,7 +141,6 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     CheckBoxPreference mCrtOff;
     ListPreference mHideStatusBar;
     ListPreference mHiddenStatusbarPulldownTimeout;
-    ListPreference mFontsize;
 
     private AnimationDrawable mAnimationPart1;
     private AnimationDrawable mAnimationPart2;
@@ -286,11 +284,6 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
                 com.android.internal.R.bool.config_unplugTurnsOnScreen)) {
             ((PreferenceGroup) findPreference(PREF_DISPLAY)).removePreference(mWakeUpWhenPluggedOrUnplugged);
         }
-
-        mFontsize = (ListPreference) findPreference("status_bar_fontsize");
-        mFontsize.setOnPreferenceChangeListener(this);
-        mFontsize.setValue(Integer.toString(Settings.System.getInt(mContentRes,
-                Settings.System.STATUSBAR_FONT_SIZE, STOCK_FONT_SIZE)));
 
         if (isTablet(mContext)) {
             mStatusbarSliderPreference.setEnabled(false);
@@ -989,12 +982,6 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
             int val = Integer.parseInt((String) newValue);
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.HIDDEN_STATUSBAR_PULLDOWN_TIMEOUT, val);
-            Helpers.restartSystemUI();
-            return true;
-        } else if (preference == mFontsize) {
-            int val = Integer.parseInt((String) newValue);
-            Settings.System.putInt(mContentRes,
-                    Settings.System.STATUSBAR_FONT_SIZE, val);
             Helpers.restartSystemUI();
             return true;
         }
